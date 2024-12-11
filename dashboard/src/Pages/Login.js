59bb963346fb210fc1276d6f8faa9805bcd3b4a2
Aaron Reid <aaron.reid@edu.sait.ca>
2024-12-11 03:59:50 -0700

    starting to add to add the front end

---
+++ dashboard/src/Pages/Login.js
@@ -0,0 +1,17 @@
+const Login = () => {
+    return ( 
+        <form action="">
+        <label>Email:</label>
+        <input type="email"
+        //value={email}
+         />
+         <label>Password:</label>
+         <input type="password"
+         //value={password}
+         />
+        </form>
+        
+     );
+}
+ 
+export default Login;
